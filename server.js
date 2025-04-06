039c69b1b385ac790731ee3011b0e3596d0a4274
Adithya GV <160870256+adithyagv@users.noreply.github.com>
2025-04-06 19:20:11 +0530

    Update server.js 1

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const multer = require("multer");
 const path = require("path");
 const axios = require("axios");
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 const nodemailer = require("nodemailer");
 const dotenv = require("dotenv");
 app.use(bodyParser.json());
